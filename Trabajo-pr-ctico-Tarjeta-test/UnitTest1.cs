f5ce2815ce254e7af4437b9b2bfcc3d104b8ed44
MarcoSeri <marcoseri2006@gmail.com>
2024-10-23 17:07:39 -0300

    Cambie a .Net devuelta

---
+++ Trabajo-pr-ctico-Tarjeta-test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Trabajo_pr_ctico_Tarjeta_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
